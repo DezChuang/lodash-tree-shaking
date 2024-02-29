afeabc0fc19bc70cdd8415101da19a592767a20f
CK Chuang <dissaivent@gmail.com>
2024-02-29 11:03:28 +0800

    try dynamic import issue

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ const loadUtilA = () => import('./util/a');
 const loadUtilB = () => import('./util/b');
 
 const process = async (a, b) => {
-    const utilModule = await loadUtilB();
-    return utilModule.add(a, b) / utilModule.add(a, b);
+
+    let utilModule;
+    if (Math.random() > 0.5) {
+        utilModule = await loadUtilA();
+        return utilModule.multiply(a, b) / utilModule.multiply(a, b);
+    } else {
+        utilModule = await loadUtilB();
+        return utilModule.add(a, b) / utilModule.add(a, b);
+    }
 };
 
 console.log(process(1, 2));
